74c72a7c114f9e0c095af2e9e1e01daabab63de3
Rada Bogdan Raul <raoul_ice@yahoo.com>
2016-08-19 11:50:26 +0300

    added some explanation about registration of engines, transformers and mime types

--- lib/sprockets/sass.rb
+++ lib/sprockets/sass.rb
@@ -40,15 +40,42 @@ module Sprockets
   rescue LoadError; end
 
   if Sprockets::Sass.version_of_sprockets >= 3
+    # We need this only for Sprockets > 3 in order to be able to register anything.
+    # For Sprockets 2.x , although the file and the module name exist,
+    # they can't be used because it will give errors about undefined methods, because this is included only on Sprockets::Base
+    # and in order to use them we would have to subclass it and define methods to expire cache and other methods for registration ,
+    # which are not needed since Sprockets already  knows about that using the environment instead internally
     require 'sprockets/processing'
     extend Sprockets::Processing
+
+    # We need this for Sprockets > 3 for the ERB processor to work with SASS and SCSS also , besides CSS
     register_mime_type 'application/scss+ruby', extensions: ['.scss.erb', '.css.scss.erb']
     register_mime_type 'application/sass+ruby', extensions: ['.sass.erb', '.css.sass.erb']
+
+    # The new way of registering the compressor, uses a name given as a symol, instead of using the class directly
+    # e.g
+    #
+    #  Rails.application.configure do |config|
+    #    config.css_compressor = Sprockets::Sass::Compressor # This works only for Sprockets 2.x
+    #     config.css_compressor = :sprockets_sass # This works with Sprockets > 3
+    #   end
+    #
     register_compressor 'text/css', :sprockets_sass, Sprockets::Sass::Compressor
   end
 
+  # We use the register engine for Sprockets 2.x and 3.x because they work like preprocessors
+  # and both version are using tex/css as a mime type for SASS and SCSS so we are using same mime type here also
+  #
+  # Sprockets 4 doesn't know about register_engine and uses transformers instead for SASS and SCSS
+  # and uses different mime types for SASS and SCSS
   if respond_to?(:register_engine)
     if respond_to?(:register_transformer)
+      # We need to do this only for Sprockets 3.x since the Erb Processor is now registered only for CSS but no SASS or SCSS
+      # this works well, because the importer knows how to call the transformers
+      # and other processors and other engines when evaluating a filename
+      # @see Sprockets::Sass:Importer#evaluate
+      # @see Sprockets::Sass:Importer#process
+      #
       register_transformer 'application/scss+ruby', 'text/css', Sprockets::ERBProcessor
       register_transformer 'application/sass+ruby', 'text/css', Sprockets::ERBProcessor
     end
@@ -58,13 +85,25 @@ module Sprockets
     args = ['.scss', Sprockets::Sass::ScssTemplate]
     args << { mime_type: 'text/css', extensions: ['.scss', '.css.scss'], silence_deprecation: true } if Sprockets::Sass.version_of_sprockets >= 3
     register_engine(*args)
-  else # Sprockets 4
+  else
+    #  THis code is executed only for Sprockets 4
+    # Sprockets 4 introduces new mime types text/sass and text/scss
+    # and uses transformers to transform to css
+
+    # The mime types are already registered, but added here just in case , to have some history about what we used
     register_mime_type 'text/sass', extensions: ['.sass', '.css.sass']
     register_mime_type 'text/scss', extensions: ['.scss', '.css.scss']
+
+    # Since Sprockets 4 introduces the text/sass and text/scss , this transfomer needs to use the new mime types instead of text/css
     register_transformer 'application/scss+ruby', 'text/scss', Sprockets::ERBProcessor
     register_transformer 'application/sass+ruby', 'text/sass', Sprockets::ERBProcessor
 
-    # @TODO Find a way to use transformers instead and fix conflict with Sprockets 4 transformers
+    # @TODO [ONLY Sprockets >= 4] Find a way to use transformers instead and fix conflict with Sprockets 4 transformers,
+    # which seem to be executed before our transformers. This works somehow, but not quite,
+    # The ERBProcessor is called only after the preprocessor so we will have to explicitly call
+    # this tranformer before our preprocessor in order for this to work
+    #
+    # In previous version of Sprockets this would work, because they used engines and preprocessors
     register_preprocessor 'text/sass',  Sprockets::Sass::SassTemplate
     register_preprocessor 'text/scss', Sprockets::Sass::ScssTemplate
     register_preprocessor 'text/css',  Sprockets::Sass::SassTemplate
